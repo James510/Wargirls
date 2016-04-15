7eaf36f3d11141a57faaab752b4dd0b67cb79c21
James510 <jamesl510@yahoo.com>
2016-04-15 17:39:41 -0500

    Merge Fix

--- Assets/_Scripts/CameraMovement.cs
+++ Assets/_Scripts/CameraMovement.cs
@@ -4,11 +4,31 @@ using System.Collections;
 public class CameraMovement : MonoBehaviour
 {
     public float speed;
-    public float xMin, xMax;
+    public int x, y, z; //coordinates in the camera angle matrix
+    private Vector3 destination;//global location for the cam to go towards
 
+    void Start()
+    {
+        x = 1;
+        y = 0;
+        z = 0;
+    }
+    
 	void Update ()
     {
-        transform.Translate(new Vector3(Input.GetAxis("Horizontal") * speed *Time.deltaTime,0.0f,0.0f));
-        transform.position = new Vector3(Mathf.Clamp(transform.position.x, xMin, xMax), 5.0f, -10.0f);
+        GameController gc = GameObject.FindWithTag("GameController").GetComponent<GameController>();
+        
+        if (Input.GetKeyDown(KeyCode.A) && x > 0) x--;
+        if (Input.GetKeyDown(KeyCode.D) && x < 2) x++;
+
+        if (Input.GetKeyDown(KeyCode.W) && y == 0) y++;
+        if (Input.GetKeyDown(KeyCode.S) && y != 0) y--;
+
+        if (Input.GetKeyDown(KeyCode.Space))
+            { if (z != 0) z--; else z++; }
+
+        destination = gc.camAngles[x, y, z];
+
+        transform.position = Vector3.Lerp(transform.position, destination, speed*Time.deltaTime);
     }
 }
